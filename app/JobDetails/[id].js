f8418de5882b2b4a6db50c812f7cf518e882b243
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-23 13:28:23 +0100

    Added some dummy data...

--- app/JobDetails/[id].js
+++ app/JobDetails/[id].js
@@ -2,6 +2,8 @@ import React, { useCallback, useState } from 'react'
 import { Stack, useRouter, useSearchParams } from "expo-router";
 import { View, Text, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from "react-native";
 
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from "../../Components/Index"
+
 const JobDetail = () => {
     return (
         <View>
